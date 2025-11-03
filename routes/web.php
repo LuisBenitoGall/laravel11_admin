e5be622d3161824251ca5a0262ce7c69c36b7a01
LuisBenitoGall <luisbgall@gmail.com>
2025-11-03 23:20:05 +0100

    correcciones menu

--- routes/web.php
+++ routes/web.php
@@ -56,6 +56,7 @@ use App\Http\Controllers\Admin\CostCenterController;
 use App\Http\Controllers\Admin\CountryController;
 use App\Http\Controllers\Admin\CrmAccountController;
 use App\Http\Controllers\Admin\CrmContactController;
+use App\Http\Controllers\Admin\CrmOpportunityController;
 use App\Http\Controllers\Admin\CurrencyController;
 // use App\Http\Controllers\Admin\CustomerProductController;
 use App\Http\Controllers\Admin\CustomerProviderController;
@@ -403,6 +404,11 @@ Route::middleware(['web', 'auth', 'company'])->prefix('admin')->group(function()
     //CRM Accounts:
     Route::middleware('module_setted:crm')->group(function (){
         Route::get('/crm-contacts', [CrmContactController::class, 'index'])->name('crm-contacts.index')->middleware('permission:crm-contacts.index');
+    });
+
+    //CRM Accounts:
+    Route::middleware('module_setted:crm')->group(function (){
+        Route::get('/crm-opportunities', [CrmOpportunityController::class, 'index'])->name('crm-opportunities.index')->middleware('permission:crm-opportunities.index');
     });
 
     //Currencies:
